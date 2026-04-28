2eacefcec83d73c710d49ce019efa38a854e695a
Ujjwal ke. Singh <dujjwalsingh@gmail.com>
2026-04-28 07:08:57 +0530

    small changes

--- backend/src/utils/upload.js
+++ backend/src/utils/upload.js
@@ -1,16 +1,23 @@
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
+// ensure uploads folder exists
+const uploadDir = path.join(__dirname, "../../uploads");
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "uploads/");
+        cb(null, uploadDir);
     },
     filename: function (req, file, cb) {
         const uniqueName = Date.now() + "-" + file.originalname;
         cb(null, uniqueName);
     }
 });
-
 
 const fileFilter = (req, file, cb) => {
     const allowed = ["image/jpeg", "image/png", "image/gif"];
